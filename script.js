edcdc4048213e508d6332d8c5d670b735732d118
pruthviraj990 <128907326+pruthviraj990@users.noreply.github.com>
2025-05-13 08:53:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+function showUrlLength() {
+  const currentUrl = location.href; // Get the full current URL
+  const urlLength = currentUrl.length; // Get the length
+  alert(`The length of the URL is: ${urlLength}`);
+}
